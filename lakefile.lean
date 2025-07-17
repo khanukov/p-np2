225dc08d6e03860e0ad56d2082d737c4a0c53d06
Dmitry Khanukov <admin@wxq.ru>
2025-07-16 20:22:16 -0400

    pnp2: add depth bound lemma and legacy tests

--- lakefile.lean
+++ lakefile.lean
@@ -22,5 +22,5 @@ lean_exe tests where
 
 @[test_driver]
 lean_lib Tests where
-  globs := #[`Basic, `CoverExtra, `Migrated]
+  globs := #[`Basic, `CoverExtra, `Migrated, `Pnp2Legacy]
   srcDir := "test"

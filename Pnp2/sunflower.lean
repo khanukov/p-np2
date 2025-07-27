88b402ff81f4d0cd97d6268461c26db21ddc7a24
Dmitry Khanukov <admin@wxq.ru>
2025-07-27 16:46:27 -0400

    fix sunflower re-export

--- Pnp2/sunflower.lean
+++ Pnp2/sunflower.lean
@@ -7,4 +7,4 @@ This module simply re-exports `Pnp2.Sunflower.Sunflower` under the shorter
 path `Pnp2.sunflower`.
 -/
 
-export Pnp2.Sunflower.Sunflower (IsSunflower HasSunflower sunflower_exists)
+export Sunflower (IsSunflower HasSunflower sunflower_exists)
